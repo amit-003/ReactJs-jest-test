8d3f2934aecfa7b136dfda52b5649a7afa120c5e
amit <amit.kumar@hireajackal.com>
2019-05-29 14:38:22 +0530

    added Logintest file

---
+++ src/components/login/Login.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from '../../components/login/Login'
+
+describe('Test case for testing loginform', () => {
+	test('username check', () => {;
+		const component = ( < Login / > );
+
+
+	})
+
+});
